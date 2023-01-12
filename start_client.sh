00be17262b4df33871383049acc4713f2725144f
Franco Liberali <franco.liberali@gmail.com>
2023-01-12 10:28:17 +0100

    add code from quiche with modifications

---
+++ start_client.sh
@@ -0,0 +1,1 @@
+cargo run --bin quiche-client -- https://127.0.0.1:4433 --no-verify --dgram-proto siduck --dgram-count 3
---
+++ start_server.sh
@@ -0,0 +1,1 @@
+cargo run --bin quiche-server -- --cert apps/src/bin/cert.crt --key apps/src/bin/cert.key --dgram-proto siduck
