3d764eadb65b1bd8bf822fe8a46d8fd14dec87a0
Franco Liberali <franco.liberali@gmail.com>
2023-01-26 15:01:46 +0100

    move buffer size to begining

--- test_tcp_receiver.py
+++ test_tcp_receiver.py
@@ -2,6 +2,8 @@ import socket
 
 BIND_IP = "127.0.0.1"
 BIND_PORT = 3333
+BUFFER_SIZE = 2048
+
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind((BIND_IP, BIND_PORT))
 # become a server socket
@@ -11,8 +13,6 @@ print("Listening on %s:%s" % (BIND_IP, BIND_PORT))
 
 (clientsocket, address) = serversocket.accept()
 print("Connected from ", address)
-
-BUFFER_SIZE = 2048
 
 while True:
     data = clientsocket.recv(BUFFER_SIZE)
