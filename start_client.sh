75cb01a21211a27c69580ce045c5b6a01048d381
Franco Liberali <franco.liberali@gmail.com>
2023-01-27 18:11:10 +0100

    change init scripts to use mp

--- start_client.sh
+++ start_client.sh
@@ -1,1 +1,1 @@
-RUST_LOG=info cargo run --bin quiche-client -- https://127.0.0.1:4433 --no-verify --dgram-proto siduck
+RUST_LOG=info cargo run --bin quiche-client -- https://127.0.0.1:4433 --no-verify --dgram-proto siduck --multipath --address 127.0.0.1:8888 --address 127.0.0.1:9999
--- start_server.sh
+++ start_server.sh
@@ -1,1 +1,1 @@
-RUST_LOG=info cargo run --bin quiche-server -- --cert src/bin/cert.crt --key src/bin/cert.key --dgram-proto siduck
+RUST_LOG=info cargo run --bin quiche-server -- --cert src/bin/cert.crt --key src/bin/cert.key --multipath --dgram-proto siduck
