f0e1dadd443900e90e96dbd59f38f16f3d0e42d5
Franco Liberali <franco.liberali@gmail.com>
2023-02-16 09:53:09 +0100

    use --listen param in start server

--- start_server.sh
+++ start_server.sh
@@ -1,1 +1,1 @@
-RUST_LOG=info cargo run --bin quiche-server -- --cert src/bin/cert.crt --key src/bin/cert.key --multipath --dgram-proto siduck --max-active-cids 4
+RUST_LOG=info cargo run --bin quiche-server -- --listen 127.0.0.1:4433 --cert src/bin/cert.crt --key src/bin/cert.key --multipath --dgram-proto siduck --max-active-cids 4
